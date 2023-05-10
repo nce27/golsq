82a6a321da8c28ac017cde3fcd359fea09846b69
nce27 <nc3elliott@gmail.com>
2023-05-10 15:24:00 +0100

    commit: Changed initial reminder class and spec file for error testing

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -2,12 +2,14 @@ class Reminder
   def initialize(name)
     @name = name
   end
-  
+
   def remind_me_to(task)
-    @task  = task
+    @task = task
   end
 
   def remind()
-    return "#{@task}, #{@name}"
+    # Look here! We want to fail if there is no reminder yet.
+    fail "No reminder set!" if @task.nil?
+    return "#{@task}, #{@name}!"
   end
 end
--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -1,10 +1,11 @@
 require 'reminder'
 
 RSpec.describe Reminder do
-  it "reminds the user to do a task" do
-    reminder = Reminder.new("Kay")
-    reminder.remind_me_to("Walk the dog")
-    result = reminder.remind()
-    expect(result).to eq "Walk the dog, Kay"
+  context "when no task is set" do
+    it "fails" do
+      reminder = Reminder.new("Kay")
+      #result = reminder.remind()
+      expect{  reminder.remind() }.to raise_error "No reminder set!"
+    end
   end
 end
